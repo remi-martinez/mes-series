0cc70fac54ff56b8f85d248611247f58b03226fa
Rémi Martinez <64494563+remi-martinez@users.noreply.github.com>
2020-11-03 18:21:53 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$db=new PDO("mysql:host=91.216.107.219; dbname=regow1442465;charset=utf8", "regow1442465", "xmQBaXJB5");
+	$db=new PDO("mysql:host=91.216.107.219; dbname=regow1442465;charset=utf8", "regow1442465", "PASSWORD");
 }
 catch(Exception $e)
 {
